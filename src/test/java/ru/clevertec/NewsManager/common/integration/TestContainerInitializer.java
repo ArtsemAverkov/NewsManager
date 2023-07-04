639849f82af2012ca8d9792db3dd853a1577bfbe
ArtsemAverkov <102361520+ArtsemAverkov@users.noreply.github.com>
2023-07-04 22:28:41 +0300

    fix: start test conteiner

--- src/test/java/ru/clevertec/NewsManager/common/integration/TestContainerInitializer.java
+++ src/test/java/ru/clevertec/NewsManager/common/integration/TestContainerInitializer.java
@@ -1,17 +1,32 @@
 package ru.clevertec.NewsManager.common.integration;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import ru.clevertec.NewsManager.common.WireMockInitializer;
+
 
 /**
  Initializes the test container for PostgreSQL and sets the required properties for data source configuration.
  */
 
 public class TestContainerInitializer {
-    private static final DockerImageName POSTGRES_IMAGE = DockerImageName.parse("postgres:14.3");
+
+    @BeforeAll
+    static void setup() {
+        WireMockInitializer.setup();
+    }
+
+    @AfterAll
+    static void teardown() {
+        WireMockInitializer.teardown();
+    }
+
+    private static final DockerImageName POSTGRES_IMAGE =
+            DockerImageName.parse("postgres:14.3");
 
 
     private static PostgreSQLContainer<?> postgresContainer;
@@ -33,6 +48,5 @@ public class TestContainerInitializer {
         registry.add("spring.datasource.password", postgresContainer::getPassword);
         registry.add("spring.datasource.username", postgresContainer::getUsername);
     }
-
 }
 
